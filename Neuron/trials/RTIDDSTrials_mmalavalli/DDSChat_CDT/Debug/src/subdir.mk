52ffa067afbb45b4071fd096f009f4b1c36f5ad2
Manjesh Malavalli <mmalavalli@xvdth.com>
2010-07-26 18:22:26 +0000

    DDSChat Eclipse Project [Trial to get DDS working with Eclipse]

---
+++ Neuron/trials/RTIDDSTrials_mmalavalli/DDSChat_CDT/Debug/src/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/DDSChatModule.cpp \
+../src/main.cpp 
+
+CXX_SRCS += \
+../src/DDSChat.cxx \
+../src/DDSChatPlugin.cxx \
+../src/DDSChatSupport.cxx 
+
+OBJS += \
+./src/DDSChat.o \
+./src/DDSChatModule.o \
+./src/DDSChatPlugin.o \
+./src/DDSChatSupport.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/DDSChatModule.d \
+./src/main.d 
+
+CXX_DEPS += \
+./src/DDSChat.d \
+./src/DDSChatPlugin.d \
+./src/DDSChatSupport.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cxx
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DRTI_UNIX -I/home/manjesh/RTIDDS/RTI/ndds.4.5c/include -I/home/manjesh/RTIDDS/RTI/ndds.4.5c/include/ndds -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/DDSChat: ../src/DDSChat.idl
+	@echo 'Building file: $<'
+	@echo 'IDL Compiler ...'
+	/home/manjesh/RTIDDS/RTI/ndds.4.5c/scripts/rtiddsgen -d ../src -replace ../src/DDSChat.idl
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DRTI_UNIX -I/home/manjesh/RTIDDS/RTI/ndds.4.5c/include -I/home/manjesh/RTIDDS/RTI/ndds.4.5c/include/ndds -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
