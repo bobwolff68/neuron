2a374f4a5b023643ce10220255fdae2835a2710c
Manjesh Malavalli <mmalavalli@xvdth.com>
2010-11-05 19:16:35 +0000

    updated to have a higher level internal neuron common header to shield internal details of which headers to include to make neuroncommon work for us.

---
+++ Neuron/code/include/neuroncommon.h
@@ -0,0 +1,9 @@
+//
+// Include for internal use of neuron common library
+//
+
+#include "../neuroncommon/threading/NeuronBasics/ThreadSingle.h"
+#include "../neuroncommon/threading/NeuronBasics/ThreadMultiple.h"
+
+#include "../neuroncommon/eventhandling/eventhandler.h"
+
