d5756a096e151245b8f19294fcf2a5bd8df81f44
Manjesh Malavalli <mmalavalli@xvdth.com>
2010-12-08 00:42:32 +0000

    Control Plane Update

--- Neuron/code/control/include/controlplane.h
+++ Neuron/code/control/include/controlplane.h
@@ -11,12 +11,16 @@
 #include "scp/SCPEvent.h"
 #include "scp/SCPMaster.h"
 #include "scp/SCPSlave.h"
+#define SCP_MASTER_NAME		"SCPMaster"
+#define	SCP_SLAVE_NAME		"SCPSlave"
 
 #include "lscp/idl/LSCPInterface.h"
 #include "lscp/idl/LSCPInterfaceSupport.h"
 #include "lscp/LSCPEvent.h"
 #include "lscp/LSCPMaster.h"
 #include "lscp/LSCPSlave.h"
+#define LSCP_MASTER_NAME	"LSCPMaster"
+#define LSCP_SLAVE_NAME		"LSCPSlave"
 
 #include "ecp/idl/ECPInterface.h"
 #include "ecp/idl/ECPInterfaceSupport.h"
@@ -29,5 +33,14 @@
 #include "acp/ACPEvent.h"
 #include "acp/ACPMaster.h"
 #include "acp/ACPSlave.h"
+#define ACP_MASTER_NAME	"ACPMaster"
+#define ACP_SLAVE_NAME	"ACPSlave"
+
+#define	GEN_CP_INTERFACE_NAME(name,ownerName,CPInterfaceTypeName)\
+		{\
+			strcpy(name,ownerName);\
+			strcat(name,"::");\
+			strcat(name,CPInterfaceTypeName);\
+		}
 
 #endif
