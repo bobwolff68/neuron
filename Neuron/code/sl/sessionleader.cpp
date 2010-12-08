e21e15247a062b523aecb6f864869f132784b1dd
Manjesh Malavalli <mmalavalli@xvdth.com>
2010-12-08 01:17:34 +0000

    Updated to instantiate NatNumSrc and StdOutSink

--- Neuron/code/sl/sessionleader.cpp
+++ Neuron/code/sl/sessionleader.cpp
@@ -3,36 +3,250 @@
 #include <unistd.h>
 #include "sessionleader.h"
 
-SessionLeader::SessionLeader(IDType slIdParam) : EventHandlerT<SessionLeader>(),ThreadSingle()
+SessionLeader::SessionLeader(IDType slIdParam,IDType sIdParam,const char *nameParam,int domIdParam,
+							 int ownerIdParam) : EventHandlerT<SessionLeader>(),ThreadSingle()
 {
-	id = slIdParam;
+	DDS_ReturnCode_t	retCode;
+	char				LSCPSlaveName[100];
+	const char 		   *typeName = NULL;
 	
-	std::cout << "Created SL for session " << id << endl;
+	id = slIdParam;
+	sessionId = sIdParam;
+	ownerId = ownerIdParam;
+	strcpy(name,nameParam);
+	
 	// Create Local Session Control Plane (LSCP) slave
+	GEN_CP_INTERFACE_NAME(LSCPSlaveName,name,LSCP_SLAVE_NAME);
+	pLSCSlave = new LSCPSlave(this,id,domIdParam,LSCPSlaveName,"LSCP");
+	pLSCSlaveObj = pLSCSlave->CreateSlaveObject(sessionId);
+	
+	// Create Entity Control Plane
+	
 	
 	// Generate hash table of Event Handle Function Ptrs with Event kind as key
-	//AddHandleFunc(&SessionLeader::HandleNewSessionEvent,EVENT_KIND_NEW_SESSION);
-	//AddHandleFunc(&SessionLeader::HandleUpdateSessionEvent,EVENT_KIND_UPDATE_SESSION);
-	//AddHandleFunc(&SessionLeader::HandleDeleteSessionEvent,EVENT_KIND_DELETE_SESSION);
+	AddHandleFunc(&SessionLeader::HandleNewSessionEvent,LSCP_EVENT_NEW_SESSION);
+	AddHandleFunc(&SessionLeader::HandleUpdateSessionEvent,LSCP_EVENT_UPDATE_SESSION);
+	AddHandleFunc(&SessionLeader::HandleDeleteSessionEvent,LSCP_EVENT_DELETE_SESSION);
+
+    // Initialize LSCP instances
+    control = com::xvd::neuron::lscp::ControlTypeSupport::create_data();
+    state = com::xvd::neuron::lscp::StateTypeSupport::create_data();
+    event = com::xvd::neuron::lscp::EventTypeSupport::create_data();
+    metrics = com::xvd::neuron::lscp::MetricsTypeSupport::create_data();
+    
+    //Init media plane
+    pMediaDP = DDSTheParticipantFactory->create_participant(domIdParam,DDS_PARTICIPANT_QOS_DEFAULT,
+    														NULL,DDS_STATUS_MASK_NONE);
+    if(pMediaDP==NULL)
+    {
+    	std::cout << "Cannot create domain participant" << std::endl;
+    	exit(0);
+    }
+    typeName = com::xvd::neuron::media::DataUnitTypeSupport::get_type_name();
+    retCode = com::xvd::neuron::media::DataUnitTypeSupport::register_type(pMediaDP,typeName);
+    if(retCode!=DDS_RETCODE_OK)
+    {
+        std::cout << "Cannot register type" << std::endl;
+        retCode = DDSTheParticipantFactory->delete_participant(pMediaDP);
+        if(retCode!=DDS_RETCODE_OK)
+        	std::cout << "Cannot delete domain participant" << std::endl;
+        exit(0);
+    }
+    
+    SetStateStandby();
+}
+
+SessionLeader::~SessionLeader()
+{
+	DDS_ReturnCode_t	retCode;
 	
-	// Send setup success to session factory
+	//Delete media plane
+	std::map<int,SessionEntity*>::iterator it;
+	for(it=EntityList.begin(); it!=EntityList.end(); it++)
+	{
+		switch(it->second->GetKind())
+		{
+			case ENTITY_KIND_NATNUMSRC:
+				
+				((NatNumSrc *)(it->second))->stopThread();
+				delete (NatNumSrc *)(it->second);
+				break;
+				
+			case ENTITY_KIND_STDOUTSINK:
+
+				((StdOutSink *)(it->second))->stopThread();
+				delete (StdOutSink *)(it->second);
+				break;
+				
+			default:
+			
+				std::cout << "Entity kind " << it->second->GetKind() <<" not valid" << std::endl;
+				break;
+		}
+		
+		std::cout << "Deleted entity " << it->first << std::endl;
+		retCode = pMediaDP->delete_topic(TopicList[it->first]);
+		if(retCode!=DDS_RETCODE_OK)
+		{
+			std::cout << "Cannot delete topic" << std::endl;
+			exit(0);
+		}
+	}
+	EntityList.clear();
+	TopicList.clear();
+	retCode = DDSTheParticipantFactory->delete_participant(pMediaDP);
+	if(retCode!=DDS_RETCODE_OK)
+	{
+		std::cout << "Cannot delete domain participant" << std::endl;
+		exit(0);
+	}
+	
+	SetStateDeleted();
+	pLSCSlave->DeleteSlaveObject(pLSCSlaveObj);
+	delete pLSCSlave;
+
+    // Delete LSCP instances
+    com::xvd::neuron::lscp::ControlTypeSupport::delete_data(control);
+    com::xvd::neuron::lscp::StateTypeSupport::delete_data(state);
+    com::xvd::neuron::lscp::EventTypeSupport::delete_data(event);
+    com::xvd::neuron::lscp::MetricsTypeSupport::delete_data(metrics);
 }
 
 void SessionLeader::HandleNewSessionEvent(Event *pEvent)
 {
-	std::cout << "Created SessionSL object for session " << id << endl;
+	LSCPEventNewSession 	*pNewSessEvt = NULL;
+	LSCPSlaveObject			*pLSCSlaveObjLocal = NULL;
+	
+	pNewSessEvt = reinterpret_cast<LSCPEventNewSession*>(pEvent);
+	pLSCSlaveObjLocal = (LSCPSlaveObject*)pNewSessEvt->GetSession();
+	
+	if(pLSCSlaveObj!=NULL)
+	{
+        if(pLSCSlaveObjLocal->GetSessionId()==sessionId)
+        {
+        	strcpy(control->script,pNewSessEvt->GetData()->script);
+        	SetStateReady();
+        }
+	}
+	else
+	{
+		std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": Null LSCP slave object" << endl;
+		pLSCSlave->DeleteSlaveObject(pLSCSlaveObjLocal);
+	}
+	
 	return;
 }
 
 void SessionLeader::HandleUpdateSessionEvent(Event *pEvent)
 {
-	std::cout << "Updated SessionSL object for session " << id << endl;
+	LSCPEventUpdateSession	*pUpSessEvt = NULL;
+	
+	if(pLSCSlaveObj!=NULL)
+	{
+		pUpSessEvt = reinterpret_cast<LSCPEventUpdateSession*>(pEvent);
+		if(pUpSessEvt->GetData()->sessionId==sessionId)
+		{
+			Update(pUpSessEvt->GetData());
+			SetStateReady();
+			ProcessScript(control->script);
+		}
+	}
+	else
+		std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": Null LSCP slave object" << endl;
+	
 	return;
 }
 
 void SessionLeader::HandleDeleteSessionEvent(Event *pEvent)
 {
-	std::cout << "Deleted SessionSL object for session " << id << endl;
+	std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": Deleting" << std::endl;
+	//stopThread();
+	return;
+}
+
+void SessionLeader::ProcessScript(const char *script)
+{
+	int 	cmdId;
+	int 	entityId;
+	int 	entityType;
+	int 	srcId;
+	char	topicName[50];
+	
+	sscanf(script,"\"%d,%d,%d,%d\"",&cmdId,&entityId,&entityType,&srcId);
+	switch(cmdId)
+	{
+		case 1:	//ADDENTITY
+		
+			MEDIA_TOPIC_NAME(topicName,srcId);
+			TopicList[entityId] = pMediaDP->create_topic(topicName,
+														 com::xvd::neuron::media::DataUnitTypeSupport::get_type_name(),
+														 DDS_TOPIC_QOS_DEFAULT,NULL,
+														 DDS_STATUS_MASK_NONE);
+			if(TopicList[entityId]!=NULL)
+			{
+				switch(entityType)
+				{
+					case ENTITY_KIND_NATNUMSRC:
+					{
+						NatNumSrc *pSrc = new NatNumSrc(entityId,id,sessionId,20,1,5,pMediaDP,TopicList[entityId]);				
+						pSrc->startThread();
+						EntityList[entityId] = (SessionEntity *)(pSrc);
+						std::cout << "Kind=" << EntityList[entityId]->GetKind() << std::endl;
+						break;
+					}
+					
+					case ENTITY_KIND_STDOUTSINK:
+					{	
+						StdOutSink *pSink = new StdOutSink(entityId,srcId,id,sessionId,pMediaDP,TopicList[entityId],"*");
+						pSink->startThread();
+						EntityList[entityId] = (SessionEntity *)(pSink);
+						std::cout << "Kind=" << EntityList[entityId]->GetKind() << std::endl;
+						break;
+					}
+					
+					default:
+				
+						std::cout << "Entity kind " << entityType << " not valid" << std::endl;
+						break;
+				}
+			}
+			else
+				std::cout << "Cannot create topic" << std::endl;
+			
+			break;
+			
+		case 2: //DELETEENTITY
+			
+			switch(entityType)
+			{
+				case ENTITY_KIND_NATNUMSRC:
+					
+					delete (NatNumSrc *)(EntityList[entityId]);
+					break;
+					
+				case ENTITY_KIND_STDOUTSINK:
+				
+					delete (StdOutSink *)(EntityList[entityId]);
+					break;
+					
+				default:
+				
+					std::cout << "Entity kind " << entityType << " not valid" << std::endl;
+					break;
+			}
+			
+			EntityList.erase(entityId);
+			DDS_ReturnCode_t retCode = pMediaDP->delete_topic(TopicList[entityId]);
+			if(retCode!=DDS_RETCODE_OK)
+			{
+				std::cout << "Cannot delete topic" << std::endl;
+				exit(0);
+			}
+			TopicList.erase(entityId);
+			
+			break;
+	}
+	
 	return;
 }
 
--- Neuron/code/sl/sessionleader.h
+++ Neuron/code/sl/sessionleader.h
@@ -1,7 +1,16 @@
 #ifndef SESSIONLEADER_H_
 #define SESSIONLEADER_H_
 
+#include <string.h>
+#include "ndds_cpp.h"
 #include "neuroncommon.h"
+#include "controlplane.h"
+#include "natnumsrc.h"
+#include "stdoutsink.h"
+
+#define DOMAIN_ID_LSCP	11
+
+#define SL_LOG_PROMPT(oId,id)	"SF(" << oId << ")->SL(" << id << ")"
 
 typedef	long long IDType;
 
@@ -9,9 +18,21 @@ class SessionLeader : public EventHandlerT<SessionLeader>, public ThreadSingle
 {
 	private:
 	
-		IDType	id;
-		// Local Session Ctrl Object (Slave)
-		// List of Entities
+		IDType				id;
+		IDType				sessionId;
+		IDType				ownerId;
+		char		   	   	name[100];
+		LSCPSlave	   	   *pLSCSlave;
+		LSCPSlaveObject	   *pLSCSlaveObj;
+		
+		DDSDomainParticipant		   *pMediaDP;
+		std::map<int,SessionEntity*> 	EntityList;
+		std::map<int,DDSTopic*>			TopicList;
+		
+		com::xvd::neuron::lscp::Control *control;
+    	com::xvd::neuron::lscp::State   *state;
+    	com::xvd::neuron::lscp::Event   *event;
+    	com::xvd::neuron::lscp::Metrics *metrics;		
 		
 		void	EventHandleLoop (void);
 		int		workerBee		(void)	{ EventHandleLoop(); return 0; }
@@ -21,14 +42,70 @@ class SessionLeader : public EventHandlerT<SessionLeader>, public ThreadSingle
 		// Session Control Plane Slave Event Handlers
 		void	HandleNewSessionEvent		(Event *);
 		void	HandleUpdateSessionEvent	(Event *);
-		void	HandleDeleteSessionEvent	(Event *);	
+		void	HandleDeleteSessionEvent	(Event *);
+		void	ProcessScript				(const char *);
 
 	public:
 	
-		SessionLeader(IDType);
-		~SessionLeader()		{ std::cout << "Deleted SL for session " << id << endl; }
+		SessionLeader(IDType,IDType,const char *,int,int);
+		~SessionLeader();
 		
-		IDType	GetId(void)		{ return id; }
+		IDType	GetId(void)			{ return id; }
+
+		IDType	GetSessionId(void)	{ return sessionId; }
+		
+    	void Update(com::xvd::neuron::lscp::Control *control)
+    	{
+		    strcpy(this->control->script,control->script);
+		    std::cout << SL_LOG_PROMPT(ownerId,sessionId) << "(update): " << control->script << endl;
+		     
+		    return;
+    	}
+    	
+    	void SetStateStandby(void)
+    	{
+    		state->state = com::xvd::neuron::OBJECT_STATE_STANDBY;
+		    pLSCSlaveObj->Send(state);
+		    std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": STDBY (LSCP)" << endl; 
+		    
+		    return;
+    	}
+    	
+    	void SetStateReady(void)
+    	{
+    		state->state = com::xvd::neuron::OBJECT_STATE_READY;
+		    pLSCSlaveObj->Send(state);
+		    std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": READY (LSCP)" << endl; 
+		    
+		    return;
+    	}
+    	
+    	void SetStateUpdate(void)
+    	{
+    		state->state = com::xvd::neuron::OBJECT_STATE_UPDATE;
+		    pLSCSlaveObj->Send(state);
+		    std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": UPDATE (LSCP)" << endl; 
+		    
+		    return;
+    	}
+    	
+    	void SetStateDelete(void)
+    	{
+    		state->state = com::xvd::neuron::OBJECT_STATE_DELETE;
+		    pLSCSlaveObj->Send(state);
+		    std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": DELETE (LSCP)" << endl; 
+		    
+		    return;
+    	}
+
+    	void SetStateDeleted(void)
+    	{
+    		state->state = com::xvd::neuron::OBJECT_STATE_DELETED;
+		    pLSCSlaveObj->Send(state);
+		    std::cout << SL_LOG_PROMPT(ownerId,sessionId) << ": DELETED (LSCP)" << endl; 
+		    
+		    return;
+    	}
 };
 
 #endif /* SESSIONLEADER_H_ */
