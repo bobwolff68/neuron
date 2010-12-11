b42a11591937ae2e367a5ff646a9d19b3c8e5294
Manjesh Malavalli <mmalavalli@xvdth.com>
2010-12-11 01:51:22 +0000

    Conflict resolved

--- Neuron/code/include/neuroncommon.h
+++ Neuron/code/include/neuroncommon.h
@@ -10,14 +10,8 @@
 //
 #include "threading/NeuronBasics/ThreadSingle.h"
 #include "threading/NeuronBasics/ThreadMultiple.h"
-
 #include "eventhandling/eventhandler.h"
-
-// When DDS items will only complicate a build...they can be skipped.
-#ifndef SKIP_DDS
 #include "dds/DDSEvent.h"
-#endif
-
 #include "anyoption/anyoption.h"
 
 #include <string>
@@ -28,7 +22,7 @@ using namespace std;
 /// \brief FromString<> is a template for converting the frontmost 'word' of a string
 ///        into the template type. It also allows for failure via the boolean flag.
 ///
-/// Usage -     int i = FromString<int>( stringname );
+/// Usage -     int i = FromString<int>( stringname,flag );
 template<typename T>
 T FromString( string& s, bool& bIsOK)
 {
@@ -45,3 +39,4 @@ T FromString( string& s, bool& bIsOK)
 }
 
 #endif
+
