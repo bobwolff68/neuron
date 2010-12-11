a5095ed724a9be24bf377611620537ddae4ac294
Robert Wolff <rwolff@xvdth.com>
2010-12-11 01:44:21 +0000

    Additional standard includes, using namespace std; , and a templatized 'FromString<T>' to convert strings to int/long/double/etc.

--- Neuron/code/include/neuroncommon.h
+++ Neuron/code/include/neuroncommon.h
@@ -2,6 +2,8 @@
 // Include for internal use of neuron common library
 //
 
+#ifndef NEURON_COMMON_H
+#define NEURON_COMMON_H
 //
 // RMW mod to remove relative ../neuroncommon/ pathing.
 //     Must have -I[...]code/neuroncommon/ in command line now (via CMakeLists.txt)
@@ -17,3 +19,29 @@
 #endif
 
 #include "anyoption/anyoption.h"
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+/// \brief FromString<> is a template for converting the frontmost 'word' of a string
+///        into the template type. It also allows for failure via the boolean flag.
+///
+/// Usage -     int i = FromString<int>( stringname );
+template<typename T>
+T FromString( string& s, bool& bIsOK)
+{
+	T type;
+	istringstream inStream(s);
+
+	bIsOK=true;
+
+	inStream >> type;
+	if(!inStream)
+		bIsOK=false;
+
+	return type;
+}
+
+#endif
