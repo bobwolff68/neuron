ae0c7ccd96181ec9547cd844b7c8ff45caaa16dc
Manjesh Malavalli <mmalavalli@xvdth.com>
2011-01-12 22:51:40 +0000

    Cleaned up build problems with libcurl and readline(uses ncurses) for ubrain to work on amazon ec2.

--- Neuron/code/neuroncommon/netlib/buildcurl.sh
+++ Neuron/code/neuroncommon/netlib/buildcurl.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd curl
-cmake -G "Unix Makefiles" -DCURL_ZLIB=0 -DCURL_STATICLIB=1 -DLIBRARY_OUTPUT_PATH=../..
+cmake -G "Unix Makefiles" -DCURL_ZLIB=0 -DCURL_STATICLIB=1 -DCURL_DISABLE_LDAP=1 -DLIBRARY_OUTPUT_PATH=../..
 make clean
 make libcurl
-#cp curl/lib/libcurl.a .
