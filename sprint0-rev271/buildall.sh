0037f64c9a8e5a3c1a716bca521c453ef0d4d007
Robert Wolff <rwolff@xvdth.com>
2011-04-27 23:09:08 +0000

    Added support for building oolua and removed tolua++ in main build.

--- sprint0-rev271/buildall.sh
+++ sprint0-rev271/buildall.sh
@@ -4,10 +4,13 @@ cd neuroncommon
 
 # Only re-build cppunit if needed. It is a shared-library item so it should be built once
 # per time of revision publishing.
-# sh ./buildcppunit
+# sh ./buildcppunit.sh
 
 # Very simple/fast recompile for lua bindings from C/C++ to Lua functions and classes
-sh ./buildtolua++.sh
+# Unnecessary as of April 27th. Replaced for now with OOLUA. sh ./buildtolua++.sh
+
+# Very simple/fast recompile for lua bindings from C/C++ to Lua functions and classes
+sh ./buildoolua.sh
 
 cd netlib
 sh ./buildcurl.sh
