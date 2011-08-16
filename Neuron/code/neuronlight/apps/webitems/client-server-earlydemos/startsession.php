c6be38bd765c4378e31a18fb9e5585bf10afa411
Sean OConnell <soconnell@xvdth.com>
2011-08-16 11:13:06 -0700

    changed Location to be conference1.php

--- Neuron/code/neuronlight/apps/webitems/client-server-earlydemos/startsession.php
+++ Neuron/code/neuronlight/apps/webitems/client-server-earlydemos/startsession.php
@@ -1,4 +1,7 @@
-<?php  
+<?
+    session_start();
+    require "check.php"; 
+
     // Opens a connection to a MySQL server
     $mysqli= new mysqli ("127.0.0.1", "xvdth", "12345", "xvdth" );
     
@@ -6,16 +9,16 @@
         printf("failed");
         die('Not connected : ' . mysqli_error());
     } else {
-        $sql = "insert into sessionrecord (username, ip, portv, porta) values ('Tom', '".$_POST["cURL"]."', 4000, 4001)";
+        $sql = "insert into sessionrecord (username, ip, portv, porta) values ('$_SESSION[userid]', '".$_POST["cURL"]."', 4000, 4001)";
+
         $result = mysqli_query($mysqli, $sql);
         
-//        if($result == TRUE){
-//            echo "A record has been inserted";
-//        } else {
-//            printf ("insert did not work");
-//        }
-        
+        // if($result == TRUE){
+        //     echo "A record has been inserted";
+        // } else {
+        //     printf ("insert did not work");
+        // }
         mysqli_close($mysqli);
-    }
-    header( "Location: conference1.html" );
+     }
+     header( "Location: conference1.php" );
 ?>
