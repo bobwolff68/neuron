6213c608c56f4aa495b7a89f5a829bd2f60c323d
Robert Wolff <rwolff@xvdth.com>
2011-08-23 09:25:50 -0700

    The camera interrogator was intended to be a standalone class, but due to how QTKit works with the NSApp, it has become difficult to have it be truly standalone. It will get modified after this checkin to be more of a helper-class.

---
+++ Neuron/code/neuronlight/apps/VidCapMac/CameraInterrogation.h
@@ -0,0 +1,13 @@
+//
+//  CameraInterrogation.h
+//  
+//
+//  Created by Robert Wolff on 8/22/11.
+//  Copyright 2011 XVD Technology LTD USA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CameraInterrogation : NSObject
+
+@end
---
+++ Neuron/code/neuronlight/apps/VidCapMac/CameraInterrogation.m
@@ -0,0 +1,23 @@
+//
+//  CameraInterrogation.m
+//  
+//
+//  Created by Robert Wolff on 8/22/11.
+//  Copyright 2011 XVD Technology LTD USA. All rights reserved.
+//
+
+#import "CameraInterrogation.h"
+
+@implementation CameraInterrogation
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
