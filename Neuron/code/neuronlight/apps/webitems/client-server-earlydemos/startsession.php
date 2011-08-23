e52fc5e197439ab2412b0d180a9f5cf09f81b041
Sean OConnell <soconnell@xvdth.com>
2011-08-23 10:47:48 -0700

    check count of users in session & start on page with appropriate number of plugins.

--- Neuron/code/neuronlight/apps/webitems/client-server-earlydemos/startsession.php
+++ Neuron/code/neuronlight/apps/webitems/client-server-earlydemos/startsession.php
@@ -1,6 +1,7 @@
 <?
     session_start();
-    require "check.php"; 
+    require "check.php";
+    $location = "Location: readysession.php";    
 
     // Opens a connection to a MySQL server
     $mysqli= new mysqli ("127.0.0.1", "xvdth", "12345", "xvdth" );
@@ -10,15 +11,42 @@
         die('Not connected : ' . mysqli_error());
     } else {
         $sql = "insert into sessionrecord (username, ip, portv, porta) values ('$_SESSION[userid]', '".$_POST["cURL"]."', 4000, 4001)";
-
         $result = mysqli_query($mysqli, $sql);
         
-        // if($result == TRUE){
-        //     echo "A record has been inserted";
-        // } else {
-        //     printf ("insert did not work");
-        // }
+        $sql = "select count from sessioncount";
+        $result = mysqli_query($mysqli, $sql);
+        if (!$result) {  
+            die('Invalid query: ' . mysqli_error());
+        } else {
+            $row = @mysqli_fetch_assoc($result);
+            $count = $row['count'];
+            $count++;
+            
+            $sql = "update sessioncount set count=$count";
+            $result = mysqli_query($mysqli, $sql);
+            $_SESSION['count'] = $count;
+            
+            switch ($count){
+                case 0:
+                break;
+            
+                case 1:
+                   $location = "Location: conference1.php";
+                break;
+            
+                case 2:
+                   $location = "Location: conference1.php";
+                break;
+            
+                case 3:
+                    $location = "Location: conference2.php";
+                break;            
+            
+                default:
+                break;
+            }
+        }
         mysqli_close($mysqli);
      }
-     header( "Location: conference1.php" );
+     header( $location );
 ?>
