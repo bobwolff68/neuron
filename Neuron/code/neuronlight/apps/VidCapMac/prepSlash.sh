0c11fae0bbf0a035c0fd3563f426292b2e22393b
Robert Wolff <rwolff@xvdth.com>
2011-08-29 11:33:59 -0700

    Updated for stream?.[aac|264|mp3|mpa]

--- Neuron/code/neuronlight/apps/VidCapMac/prepSlash.sh
+++ Neuron/code/neuronlight/apps/VidCapMac/prepSlash.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+echo
 echo "Preparing '/' directory for demo of application. Will require sudo password to continue."
+echo
 RETDIR=$PWD
 
 MYCFG=$PWD/rtenc_avc_settings.cfg
@@ -8,9 +10,21 @@ cd /
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-  sudo rm -f stream$i
+  sudo rm -f stream$i*
   sudo mkfifo stream$i
   sudo chmod 666 stream$i
+
+  sudo mkfifo stream$i.264
+  sudo chmod 666 stream$i.264
+
+  sudo mkfifo stream$i.aac
+  sudo chmod 666 stream$i.aac
+
+  sudo mkfifo stream$i.mp3
+  sudo chmod 666 stream$i.mp3
+
+  sudo mkfifo stream$i.mpa
+  sudo chmod 666 stream$i.mpa
 done
 
 sudo rm -f rtenc_avc_settings.cfg
