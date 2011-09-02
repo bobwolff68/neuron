9063e84cb481a8288c32f8acb098d24f94324e86
Sean OConnell <soconnell@xvdth.com>
2011-09-01 17:13:30 -0700

    adjust initial plugin display size

--- Neuron/code/neuronlight/apps/webitems/client-server-earlydemos/landing.php
+++ Neuron/code/neuronlight/apps/webitems/client-server-earlydemos/landing.php
@@ -203,19 +203,31 @@
         <?
             $_SESSION['userid'] = $MYSESSION1;
             $_SESSION['name'] = $MYSESSION2;
+            
             $_SESSION['count'] = 0;
             $_SESSION['udec'] = 1;
-            $_SESSION['cwidth'] = 320;
-            $_SESSION['cheight'] = 180;
+            
+            $_SESSION['cwidth'] = 640;
+            $_SESSION['cheight'] = 360;
+
+            $_SESSION['d0constwidth'] = 320;
+            $_SESSION['d0constheight'] = 180;
+
+            $_SESSION['d1constwidth'] = 640;
+            $_SESSION['d1constheight'] = 360;            
+            
             $_SESSION['aratio0'] = 0.5625;
             $_SESSION['dwidth0'] = 320;
             $_SESSION['dheight0'] = 180;
+            
             $_SESSION['aratio1'] = 0.5625;
-            $_SESSION['dwidth1'] = 320;
-            $_SESSION['dheight1'] = 180; 
+            $_SESSION['dwidth1'] = 640;
+            $_SESSION['dheight1'] = 360; 
+            
             $_SESSION['aratio2'] = 0.5625;
             $_SESSION['dwidth2'] = 320;
-            $_SESSION['dheight2'] = 180;            
+            $_SESSION['dheight2'] = 180;  
+            
             require "check.php"; 
             // echo "<script type='text/javascript'>var myjurl='$_SESSION[remote_addr]';</script>";
             
