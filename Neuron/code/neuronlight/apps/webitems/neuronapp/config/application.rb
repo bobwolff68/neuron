7e7b9ab9371caad7f236484fa28ea0c9f756a462
Franz Pereira <frnz.cr@gmail.com>
2011-09-21 01:54:53 -0500

    Configuring generators

--- Neuron/code/neuronlight/apps/webitems/neuronapp/config/application.rb
+++ Neuron/code/neuronlight/apps/webitems/neuronapp/config/application.rb
@@ -44,5 +44,10 @@ module Neuronapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.template_engine :slim
+      g.test_framework :rspec, :fixture => false, :views => false
+    end
   end
 end
